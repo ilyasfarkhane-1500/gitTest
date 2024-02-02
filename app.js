ae2b6ff21f6f7ec7ccb290d2be6ead7fe6efdcb6
ilyasfarkhane-1500 <ilyasilyas1500@gmail.com>
2024-02-02 11:26:24 +0100

    add

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-console.log("hi app")
-console.log("hi app1")
-console.log("hi app12")
+const fruit = "orange";
+// Output: orange
+console.log(fruit)
+//create  a variable called name and assign it the value of your name
+let name= 'John';
+
+
